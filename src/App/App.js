ebf91f7d835bde7c92f7fec89f872033cde19a94
Trond Makonese <trondation@gmail.com>
2020-05-21 09:23:08 -0600

    Data is now fecthed into the state of the app.

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    return fetch("http://localhost:3001/api/v1/reservations")
+    .then(res => res.json())
+    .then(data => {
+      this.setState({reservations: data })
+    })
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
